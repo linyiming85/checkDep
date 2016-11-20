040e3e530a72765c84a6e822d939c175439417e4
林一鸣 <400yearslove@gmail.com>
2016-11-20 22:36:15 +0800

    checkDep
    checkDep

---
+++ src/main/java/com/unicom/gitlab/MyGitLab.java
@@ -0,0 +1,38 @@
+package com.unicom.gitlab;
+
+import org.gitlab.api.GitlabAPI;
+import org.gitlab.api.models.GitlabProject;
+import org.gitlab.api.models.GitlabSession;
+import org.gitlab.api.models.GitlabUser;
+
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * Created by linym on 2016/11/20.
+ */
+public class MyGitLab {
+    private static String host="http://172.16.54.135:10080";
+    private static String username="linym";
+    private static String password="linym1985";
+    private static GitlabAPI gitlabAPI;
+    private static GitlabSession session;
+    public MyGitLab(String host, String username, String password) throws IOException {
+        this.session=GitlabAPI.connect(host,username,password);
+        this.gitlabAPI=GitlabAPI.connect(host,session.getPrivateToken());
+    }
+
+
+    public static void main(String[] strs) throws IOException {
+        MyGitLab myGitLab=new MyGitLab(host,username,password);
+        List<GitlabUser> gitlabUserList=gitlabAPI.getUsers();
+        for(GitlabUser user:gitlabUserList){
+            System.out.println(user.getName());
+        }
+        List<GitlabProject> projectList=gitlabAPI.getProjects();
+        for (GitlabProject project:projectList){
+            System.out.println("project:"+project.getName());
+        }
+
+    }
+}
